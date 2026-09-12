8a2bb7508dcb57cd44805484e590e7c9ae0d2811
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-12 14:53:13 +0530

    Add square program

---
+++ square.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,square ;
+    printf("Enter your Number = ");
+    scanf("%d",&a);
+    square=a*a;
+    printf("Square of given Number \n%d×%d = %d",a,a,square);
+    return 0 ;
+} 
