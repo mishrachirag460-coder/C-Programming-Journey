b44b65dd817a548ee8b50876b421b144dee8d37b
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-12 14:47:56 +0530

    Add input sum program

---
+++ input_sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum ;
+    printf("Enter first Number = ");
+    scanf("%d",&a);
+    printf("Enter second Number = ");
+    scanf("%d",&b);
+    sum= a+b;
+    printf("Addition of given Number \n%d+%d = %d",a,b,sum);
+    return 0 ;
+} 
