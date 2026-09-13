ba31e41fb0e8db7dedeb4b277eec4c2a08190839
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-13 21:43:18 +0530

    Add division program

---
+++ division.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,division;
+    printf("Enter first Number = ");
+    scanf("%f",&a);
+    printf("Enter second Number = ");
+    scanf("%f",&b);
+    division=a/b;
+    printf("Division of given Number \n%f ÷ %f = %f",a,b,division);
+    return 0 ;
+}
