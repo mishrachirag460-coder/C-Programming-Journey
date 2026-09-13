11e71be1f33e2341a9fcdcb69da9e6937597cc49
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-13 21:41:12 +0530

    Add multiplication program

---
+++ multiplication.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,mul;
+   printf("Enter first Number = ");
+   scanf("%d",&a);
+   printf("Enter second Number = ");
+   scanf("%d",&b);
+   mul=a*b;
+   printf("Multiplication  of given Number \n%d×%d = %d",a,b,mul);
+   return 0 ;
+}
