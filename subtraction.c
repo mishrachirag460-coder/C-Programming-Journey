484e4139e91e3c7ec4a4006aacb8f0724d0662c0
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-13 21:38:56 +0530

    Add subtraction program

---
+++ subtraction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,sub;
+   printf("Enter first Number = ");
+   scanf("%d",&a);
+   printf("Enter second Number = ");
+   scanf("%d",&b);
+   sub=a-b;
+   printf("Subtraction of given Number \n%d-%d = %d",a,b,sub);
+   return 0 ;
+}
