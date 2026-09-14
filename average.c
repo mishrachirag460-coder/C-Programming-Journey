40b189bbfa5232dd500e1cfc6dbeb006065c1493
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-14 08:06:40 +0530

    Add average program

---
+++ average.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,average;
+    printf("Enter first Number = ");
+    scanf("%f",&a);
+    printf("Enter second Number = ");
+    scanf("%f",&b);
+    average =(a+b)/2;
+    printf("Average of given Number = %f",average);
+    return 0 ;
+}
