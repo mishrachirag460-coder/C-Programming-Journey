f71a6223dc4d76ad0f9c1fd170b1cbd843f4e8a3
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-15 08:32:07 +0530

    Add simple interest program

---
+++ simple_interest.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   float p,r,t,si;
+   printf("enter your principal :");
+   scanf("%f",&p);
+   printf("enter your rate :");
+   scanf("%f",&r);
+   printf("enter your time :");
+   scanf("%f",&t);
+   si = (p*r*t)/100;
+   printf("Your Simple interest = %f ",si);
+   return 0 ;
+}
