d3aebaa19a0648a0e039c47387e6510a690f73ac
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-16 08:23:06 +0530

    Add circle circumference program

---
+++ circle_circumference.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main()
+{
+    float pi,r,Circumference;
+    pi= 3.14;
+    printf("Enter Your Radius = ");
+    scanf("%f",&r);
+    Circumference = 2*pi*r;
+    printf("Circumference of circle : %f ",Circumference);
+    return 0 ;
+
