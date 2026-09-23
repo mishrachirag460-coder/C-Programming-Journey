cd8cfaf5917dec5f84d56b60441948993d8ee236
Chirag Mishra <mishrachirag460@gmail.com>
2026-09-23 17:03:23 +0530

    Add pass or fail program

---
+++ pass_or_fail.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter  number : ");
+    scanf("%d",&n);
+    if(n>40)
+    {
+      printf("Paas");
+    }
+    else if (n==40)
+    {
+     printf("Paas");
+    }
+    else 
+    printf("Fail");
+    return 0;
+}
